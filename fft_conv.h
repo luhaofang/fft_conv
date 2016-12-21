87a0378cebeb9d8f07a7750891711b5202d42769
luhaofang <luhaofang@camera360.com>
2016-12-21 17:45:48 +0800

    first commit

---
+++ fft_conv.h
@@ -0,0 +1,31 @@
+/*
+ * ftt_conv.h
+ *
+ *  Created on: 2016-12-21
+ *      Author: seal
+ */
+
+#ifndef FFT_CONV_H_
+#define FFT_CONV_H_
+
+
+#include <fftw3.h>
+
+class fft_conv{
+
+public:
+
+	fft_conv(){
+
+	};
+
+	~fft_conv(){
+
+	};
+
+
+};
+
+
+
+#endif /* FFT_CONV_H_ */
